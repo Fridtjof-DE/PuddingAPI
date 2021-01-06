91dccbad8cf183a1753156b3fe26e923e65e08f9
Fridtjof-DE <fridtjof.goes@gmx.de>
2021-01-06 23:42:41 +0100

    Create GuildCommand.java

---
+++ src/main/java/tk/fridtjof/puddingapi/jda/GuildCommand.java
@@ -0,0 +1,25 @@
+package tk.fridtjof.puddingapi.jda;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public abstract class GuildCommand {
+
+    public GuildMessageReceivedEvent event;
+    public String commandName;
+
+    public GuildCommand(String commandName, GuildMessageReceivedEvent event) {
+        this.event = event;
+        this.commandName = commandName;
+
+        if(arguments()[0].equalsIgnoreCase(commandName)) {
+            logic();
+        }
+    }
+
+    public String[] arguments() {
+        String[] args = event.getMessage().getContentRaw().split("\\s+");
+        return args;
+    }
+
+    public abstract void logic();
+}
