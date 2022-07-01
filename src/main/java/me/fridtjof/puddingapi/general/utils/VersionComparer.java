bbe9b7af256a5d4f962594aa39761bdb5194e059
Fridtjof-De <fridtjof.goes@gmx.de>
2022-07-01 11:11:20 +0200

    Create VersionComparer.java

---
+++ src/main/java/me/fridtjof/puddingapi/general/utils/VersionComparer.java
@@ -0,0 +1,14 @@
+package me.fridtjof.puddingapi.general.utils;
+
+public class VersionComparer {
+
+    //W.I.P. TODO
+
+    public VersionComparer(String currentVersion, String newVersion) {
+
+        currentVersion = currentVersion.replaceAll("[^\\d.]", "");
+
+        //for(currentVersion.equals(""))
+        //int[]
+    }
+}
