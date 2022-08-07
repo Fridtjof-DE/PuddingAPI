f058472eddbc22b659a4e55359fda6b7ec797040
Fridtjof-De <fridtjof.goes@gmx.de>
2022-08-07 23:00:07 +0200

    disabeled

--- src/main/java/me/fridtjof/puddingapi/general/utils/VersionComparer.java
+++ src/main/java/me/fridtjof/puddingapi/general/utils/VersionComparer.java
@@ -4,11 +4,47 @@ public class VersionComparer {
 
     //W.I.P. TODO
 
-    public VersionComparer(String currentVersion, String newVersion) {
+    String installedVersion;
+    String installedVersionCore;
+    String installedVersionSnapshot;
+    String availableVersion;
+    String availableVersionCore;
+    String availableVersionSnapshot;
+    boolean isSnapshotVersion;
 
-        currentVersion = currentVersion.replaceAll("[^\\d.]", "");
+    public VersionComparer(String installedVersion, String availableVersion) {
+        this.installedVersion = installedVersion;
+        this.availableVersion = availableVersion;
+    }
 
-        //for(currentVersion.equals(""))
-        //int[]
+    public boolean newVersionAvailable() {
+
+        /*installedVersionCore = installedVersion.replace("-SNAPSHOT-[^\\d]+", "");
+        availableVersionCore = availableVersion.replace("-SNAPSHOT-[^\\d]+", "");
+
+        if(availableVersionCore.contains("SNAPSHOT")) {
+
+            installedVersionSnapshot = installedVersion.replaceAll("-", "");
+            installedVersionSnapshot = installedVersion.replace("[^\\d]+(SNAPSHOT)", "");
+
+            availableVersionSnapshot = availableVersion.replaceAll("-", "");
+            availableVersionSnapshot = availableVersion.replace("[^\\d]+(SNAPSHOT)", "");
+
+            if(Integer.parseInt(availableVersionSnapshot) > Integer.parseInt(installedVersionSnapshot)) {
+                return true;
+            }
+        }
+
+        while(!availableVersionCore.equals("")) {
+
+            if(Integer.parseInt(availableVersionCore.formatted("^[\d]*")) > Integer.parseInt(installedVersionCore.formatted("^[\d]*")) {
+                return true;
+            }
+
+            availableVersionCore = availableVersionCore.replace("^[\d]*\.?", "");
+            installedVersionCore = installedVersionCore.replace("^[\d]*\.?", "");
+        }*/
+
+        return false;
     }
 }
