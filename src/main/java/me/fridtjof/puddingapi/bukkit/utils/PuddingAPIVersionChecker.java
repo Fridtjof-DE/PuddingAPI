ca6342173657b9c7dc8e01eec5617f4e535c3fd3
Fridtjof-DE <fridtjof.goes@gmx.de>
2023-01-18 20:43:27 +0100

    Create PuddingAPIVersionChecker.java

---
+++ src/main/java/me/fridtjof/puddingapi/bukkit/utils/PuddingAPIVersionChecker.java
@@ -0,0 +1,32 @@
+package me.fridtjof.puddingapi.bukkit.utils;
+
+import me.fridtjof.puddingapi.bukkit.PuddingAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PuddingAPIVersionChecker {
+
+    public PuddingAPIVersionChecker(JavaPlugin executingPlugin, Logger logger, long expectedVersion) {
+
+        long currentPuddingVersion = PuddingAPI.getInstance().getVersion();
+
+        String expectedWouldBe = "Expected version would be: " + expectedVersion;
+
+        if(currentPuddingVersion > expectedVersion) {
+            logger.info("PuddingAPI version is newer than expected but should work fine");
+            logger.info(expectedWouldBe);
+            return;
+        }
+
+        if(currentPuddingVersion == expectedVersion) {
+            logger.info("Using correct PuddingAPI version");
+            return;
+        }
+
+        logger.warn("Detected an outdated version of PuddingAPI.");
+        logger.warn(expectedWouldBe);
+        logger.warn("Installed version is: " + currentPuddingVersion);
+        Bukkit.getPluginManager().disablePlugin(executingPlugin);
+
+    }
+}
