377c40f415152810f1d1be5f033cb0fb7a23ba58
Fridtjof_DE <38737231+Fridtjof-DE@users.noreply.github.com>
2026-01-29 21:25:57 +0100

    cleanup and making a new function using the standard logger

--- src/main/java/me/fridtjof/puddingapi/bukkit/utils/PuddingAPIVersionChecker.java
+++ src/main/java/me/fridtjof/puddingapi/bukkit/utils/PuddingAPIVersionChecker.java
@@ -6,27 +6,66 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PuddingAPIVersionChecker {
 
-    public PuddingAPIVersionChecker(JavaPlugin executingPlugin, Logger logger, long expectedVersion) {
+    //TODO also check for recommended PuddingAPI version on modrinth
 
+    // using built-in java logger
+    public PuddingAPIVersionChecker(JavaPlugin executingPlugin, long expectedVersion) {
+
+        java.util.logging.Logger logger = executingPlugin.getLogger();
         long currentPuddingVersion = PuddingAPI.getInstance().getVersion();
 
-        String expectedWouldBe = "Expected version would be: " + expectedVersion;
+        String newerThanExpectedMsg = "PuddingAPI version is newer than expected but should work fine.";
+        String expectedWouldBeMsg = "Expected version would be: " + expectedVersion;
+        String usingCorrectMsg = "Using correct PuddingAPI version.";
+        String outdatedDetectedMsg = "Detected an outdated version of PuddingAPI.";
+        String installedIsMsg = "Installed version is: " + currentPuddingVersion;
+
 
         if(currentPuddingVersion > expectedVersion) {
-            logger.info("PuddingAPI version is newer than expected but should work fine.");
-            logger.info(expectedWouldBe);
+            logger.info(newerThanExpectedMsg);
+            logger.info(expectedWouldBeMsg);
             return;
         }
 
         if(currentPuddingVersion == expectedVersion) {
-            logger.info("Using correct PuddingAPI version");
+            logger.info(usingCorrectMsg);
             return;
         }
 
-        logger.warn("Detected an outdated version of PuddingAPI.");
-        logger.warn(expectedWouldBe);
-        logger.warn("Installed version is: " + currentPuddingVersion);
+        logger.warning(outdatedDetectedMsg);
+        logger.warning(expectedWouldBeMsg);
+        logger.warning(installedIsMsg);
         Bukkit.getPluginManager().disablePlugin(executingPlugin);
+    }
 
+
+    // using unnecessary custom logger
+    @Deprecated
+    public PuddingAPIVersionChecker(JavaPlugin executingPlugin, Logger logger, long expectedVersion) {
+
+        long currentPuddingVersion = PuddingAPI.getInstance().getVersion();
+
+        String newerThanExpectedMsg = "PuddingAPI version is newer than expected but should work fine.";
+        String expectedWouldBeMsg = "Expected version would be: " + expectedVersion;
+        String usingCorrectMsg = "Using correct PuddingAPI version.";
+        String outdatedDetectedMsg = "Detected an outdated version of PuddingAPI.";
+        String installedIsMsg = "Installed version is: " + currentPuddingVersion;
+
+
+        if(currentPuddingVersion > expectedVersion) {
+            logger.info(newerThanExpectedMsg);
+            logger.info(expectedWouldBeMsg);
+            return;
+        }
+
+        if(currentPuddingVersion == expectedVersion) {
+            logger.info(usingCorrectMsg);
+            return;
+        }
+
+        logger.warn(outdatedDetectedMsg);
+        logger.warn(expectedWouldBeMsg);
+        logger.warn(installedIsMsg);
+        Bukkit.getPluginManager().disablePlugin(executingPlugin);
     }
 }
